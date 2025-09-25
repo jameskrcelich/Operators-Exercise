96014b5311623bc0c43a2c45be483c51912105ba
James Krcelich <james.krcelich@sbcglobal.net>
2025-09-24 22:51:05 -0500

    operations exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            char? operation = null;
+
+            while( operation != 'q' ) {
+                
+                switch (operation)
+            {
+                case '+':
+                    int sum = a + b;
+                    Console.WriteLine($"{a} + {b} = {sum}");
+                    break;
+                case '-':
+                    int sum = a - b;
+                    Console.WriteLine($"{a} - {b} = {sum}");
+                    break;
+                case '*':
+                    int product = a * b;
+                    Console.WriteLine($"{a} * {b} = {product}");
+                    break;
+                case '/':
+                    int quotient  = a / b;
+                    int remainder = a % b;
+                    break;
+                default:
+                    break;
+            }}
         }
     }
 }
